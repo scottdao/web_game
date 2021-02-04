68a3a0ac64a4fcfeb905ed08804392baf9782b14
jxm <jxm@ucitychina.com>
2021-02-04 17:26:26 +0800

    success

--- fileTransform/public/js/transform.js
+++ fileTransform/public/js/transform.js
@@ -6,3 +6,16 @@ document.getElementsByClassName("zh")[0].onclick=function(){
         alert("请勾选协议")
     }
 }
+// 点击切换效果
+let ddarr=document.querySelectorAll("#left dd")
+for(let i=0;i<ddarr.length;i++){
+    ddarr[i].onclick=function(event){
+        let text=event.target.innerText
+        document.getElementById("scp").innerHTML=text
+        // 清空类名
+        for(let j=0;j<ddarr.length;j++){
+            ddarr[j].className=""
+        }
+        ddarr[i].className="click"
+    }
+}
