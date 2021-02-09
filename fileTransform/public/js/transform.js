d96ebb8819f3af7f43cf9fd80fc338323a3067b6
unknown <13466484073@139.com>
2021-02-09 22:22:49 +0800

    add index

--- fileTransform/public/js/transform.js
+++ fileTransform/public/js/transform.js
@@ -1,22 +1,22 @@
 // 点击切换效果
-let ddarr=document.querySelectorAll("#left dd")
-for(let i=0;i<ddarr.length;i++){
-    ddarr[i].onclick=function(event){
-        let text=event.target.innerText
-        document.getElementById("scp").innerHTML=text
+let ddarr = document.querySelectorAll("#left dd");
+for (let i = 0; i < ddarr.length; i++) {
+    ddarr[i].onclick = function(event) {
+        let text = event.target.innerText;
+        document.getElementById("scp").innerHTML = text;
         // 清空类名
-        for(let j=0;j<ddarr.length;j++){
-            ddarr[j].className=""
+        for (let j = 0; j < ddarr.length; j++) {
+            ddarr[j].className = "";
         }
-        ddarr[i].className="click"
-    }
+        ddarr[i].className = "click";
+    };
 }
-import { API } from '/common/api.js'
-$('#uploadFile').change((v)=>{
+import { API } from "/common/api.js";
+$("#uploadFile").change((v) => {
     const files = v.target.files;
-    console.log(files)
-    API.fileUpload({
-        params:{a:123},
-        reponseType:"formData"
-    })
-})
+    console.log(files);
+    // API.fileUpload({
+    //     params:{a:123},
+    //     reponseType:"formData"
+    // })
+});
